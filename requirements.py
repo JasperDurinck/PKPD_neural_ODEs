e774becb1f83e2614bcf743a8b07ed8641a66f21
Jasper Durinck <jasper.j.durinck@gmail.com>
2025-10-21 12:06:45 +0200

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,2 +1,6 @@
-#TODO 
-numpy<2.0
+# TODO: These are the main libraries that may cause compatibility issues (NumPy <2.0 is critical as far as I am aware).
+# The versions below are tested and known to work. Will refine setup documentation soon.
+numpy==1.26.3
+scipy==1.14.0
+scikit-learn==1.4.0
+pandas==2.2.2
